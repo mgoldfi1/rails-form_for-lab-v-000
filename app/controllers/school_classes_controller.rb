4069602695efee2404eedac3870b38e8f589f5f5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-17 16:44:08 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -23,6 +23,7 @@ end
 def update
   @school_class = SchoolClass.find_by_id(params[:id])
   @school_class.update(school_class_params(:title, :room_number))
+  redirect_to school_class_path(@school_class)
 end
 
 private
