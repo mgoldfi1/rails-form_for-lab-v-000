eeb3e601952415d9fe9cd37230774e17d5bcd02d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-17 15:50:18 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,8 +9,9 @@ def new
 end
 
 def create
-  school_class = SchoolClass.new(params.require[:school_class].permit[:title, :room_number])
-  school_class.save 
+  @school_class = SchoolClass.new(params.require[:school_class].permit[:title, :room_number])
+  @school_class.save
+  redirect_to school_class_path(@school_class)
 end
 
 def edit
